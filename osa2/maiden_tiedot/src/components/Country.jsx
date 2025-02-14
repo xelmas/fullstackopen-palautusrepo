f38962a71d0759f0f133c48e471df25e74710afd
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2025-02-14 14:02:24 +0200

    Exercise 2.20

--- osa2/maiden_tiedot/src/components/Country.jsx
+++ osa2/maiden_tiedot/src/components/Country.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+const api_key = import.meta.env.VITE_SOME_KEY
+
 const Country = ({ country }) => {
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${api_key}`
+      )
+      .then((response) => {
+        setWeatherData(response.data)
+      })
+  }, [country.capital])
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -15,6 +31,19 @@ const Country = ({ country }) => {
         alt={country.flags.alt}
         style={{ width: '200px', heigth: 'auto' }}
       />
+      <h3>Weather in {country.capital}</h3>
+      {weatherData ? (
+        <>
+          <div>temperature {weatherData.main.temp} Celsius</div>
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+            style={{ width: '80px', heigth: 'auto' }}
+          />
+          <div>wind {weatherData.wind.speed} m/s</div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
