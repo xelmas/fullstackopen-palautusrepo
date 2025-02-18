031060d8149940243d6c34131a8582f27528f6b6
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2025-02-18 10:57:09 +0200

    Exercises 4.1-4.2

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const PORT = 3003
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
