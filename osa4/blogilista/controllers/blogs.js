7e8ae55d25038cf41732e74631addac02c71b08e
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2025-02-19 14:50:12 +0200

    Exercises 4.10-4.12

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -6,10 +6,22 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-  const result = await blog.save()
-  response.status(201).json(result)
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  })
+
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,71 +1,15 @@
 const { test, after, describe, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const Blog = require('../models/blog')
-
-const assert = require('node:assert')
-
 const api = supertest(app)
-
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0,
-  },
-]
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-
-  for (const blog of initialBlogs) {
-    const blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  await Blog.insertMany(helper.initialBlogs)
 })
 
 describe('blogs as json', () => {
@@ -80,7 +24,7 @@ describe('blogs as json', () => {
 describe('All blogs found', () => {
   test('there are six blogs', async () => {
     const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlogs.length)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
   test('the first blog is about React patterns', async () => {
     const response = await api.get('/api/blogs')
@@ -99,6 +43,74 @@ describe('Blogs have id', () => {
       const keys = Object.keys(blog)
       assert(keys.includes('id'))
     })
+  })
+})
+
+describe('Valid blogs are added', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      _id: '5a422bc61b54a676234d18fd',
+      title: 'A New Hope',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2020/04/05/ANewHope.html',
+      likes: 3,
+      __v: 0,
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-TYpe', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const blogTitles = response.body.map((r) => r.title)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+    assert(blogTitles.includes('A New Hope'))
+  })
+
+  test('a blog without likes is initialized to 0', async () => {
+    const newBlog = {
+      _id: '5a422bc61b54a676234d18fd',
+      title: 'A New Hope',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2020/04/05/ANewHope.html',
+      __v: 0,
+    }
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-TYpe', /application\/json/)
+
+    assert.strictEqual(response.body.likes, 0)
+  })
+
+  test('a blog without title is not added', async () => {
+    const newBlog = {
+      _id: '5a422bc61b54a676234d18fd',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2020/04/05/ANewHope.html',
+      likes: 3,
+      __v: 0,
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+  test('a blog without url is not added', async () => {
+    const newBlog = {
+      _id: '5a422bc61b54a676234d18fd',
+      title: 'A New Hope',
+      author: 'Robert C. Martin',
+      likes: 3,
+      __v: 0,
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
 })
 
