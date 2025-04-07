82875bea88acae0aae207af6ec93ea995739b1b0
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2025-04-07 16:10:32 +0300

    Fix copy-paste error Note -> Blog

--- osa4/blogilista/controllers/testing.js
+++ osa4/blogilista/controllers/testing.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
-const Note = require('../models/note')
+const Blog = require('../models/blog')
 const User = require('../models/user')
 
 router.post('/reset', async (request, response) => {
-  await Note.deleteMany({})
+  await Blog.deleteMany({})
   await User.deleteMany({})
 
   response.status(204).end()
